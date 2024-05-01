438daf0b975317f826669a48a873b443fa78c2fd
JP McDaniel <patrick@plaidjacketapps.com>
2024-05-01 11:00:40 -0500

    add comment

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,6 +24,7 @@ export default function Index() {
 
   useEffect(() => {
     const fetchPosts = async () => {
+      // avoid race condition
       abortControllerRef.current?.abort();
       abortControllerRef.current = new AbortController();
 
